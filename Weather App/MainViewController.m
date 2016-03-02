ffbf659b99556c512b206511f38295fc59c21ac9
magfurulabeer <magfurulabeer@gmail.com>
2016-03-02 01:21:00 -0500

    Successfully display main information

--- Weather App/MainViewController.m
+++ Weather App/MainViewController.m
@@ -10,7 +10,7 @@
 
 @interface MainViewController ()
 
-@property (weak, nonatomic) IBOutlet UIImageView *backgrounndImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UILabel *tempLabel;
 @property (weak, nonatomic) IBOutlet UILabel *hiTempLabel;
@@ -27,7 +27,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.apiKey = @"e9da352d7560223033ad539ccdbd1038";
-
+    
+    self.textField.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -35,8 +36,70 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - Weather API Handler Methods
+
+-(void)displayWeatherForCity:(NSString *)city {
+    
+    NSDictionary *weatherData = [self weatherDataForCity:city];
+    
+    NSLog(@"%@", weatherData);
+
+    // Set name of city in text field
+    self.textField.text = weatherData[@"name"];
+
+    // Set temperature label in Farenheight
+    NSNumber *temperature = weatherData[@"main"][@"temp"];
+    NSInteger temperatureAsInteger = [temperature integerValue];
+    self.tempLabel.text = [NSString stringWithFormat:@"%li°F", temperatureAsInteger];
+    
+    // Set the description
+    NSDictionary *weatherConditions = weatherData[@"weather"][0];
+    NSString *description = [weatherConditions[@"description"] capitalizedString];
+    self.descriptionLabel.text = [NSString stringWithFormat:@"%@", description];
+
+    NSString *iconURLString = [NSString stringWithFormat:@"http://openweathermap.org/img/w/%@.png",
+                         weatherConditions[@"icon"]];
+    NSURL *iconURL = [NSURL URLWithString:iconURLString];
+    NSData *iconData = [NSData dataWithContentsOfURL:iconURL];
+    self.iconImageView.image = [UIImage imageWithData:iconData];
+    
+//    self.imageView.image = [self imageForWeather:weatherConditions[@"main"]];
+    
+//
+//
+//
+}
+
+-(NSDictionary *)weatherDataForCity:(NSString *)city {
+    
+    NSString *requestURLString = [NSString stringWithFormat:@"http://api.openweathermap.org/data/2.5/weather?q=%@&units=imperial&APPID=%@", city, self.apiKey];
+    
+    NSURL *requestURL = [NSURL URLWithString:requestURLString];
+    
+//    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    
+    NSData *jsonData = [NSData dataWithContentsOfURL:requestURL];
+
+    NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:jsonData
+                                                                   options:kNilOptions
+                                                                     error:nil];
+    NSLog(@"%@", jsonDictionary);
+    return jsonDictionary;
+}
+
+
 #pragma mark - UITextFieldDelegate Methods
 
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSString *query = [textField.text stringByReplacingOccurrencesOfString:@" " withString:@""];
+    [self displayWeatherForCity:query];
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+#pragma mark - Miscellaneous Methods
 
 -(BOOL)prefersStatusBarHidden {
     return YES;
